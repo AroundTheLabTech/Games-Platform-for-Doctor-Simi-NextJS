874d803831e08d75864054cb7639078d0d24eda5
IvanMarte <ivanbiasfx@gmail.com>
2024-11-08 17:18:38 -0600

    update: inicio de sesion permamnete

--- src/app/page.js
+++ src/app/page.js
@@ -6,137 +6,167 @@ import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "fire
 import { doc, setDoc, updateDoc, serverTimestamp } from "firebase/firestore"; // Importar Firestore
 import { db } from "../../lib/firebase"; // Importar la instancia de Firestore configurada en firebase.js
 import { useState, useEffect, useRef } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 
 // --Animations--
 import gsap from "gsap";
 
-
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState(""); // Nuevo estado para el nombre
-  const [location, setLocation] = useState("");
-  const [gender, setGender] = useState("");
-  const [age, setAge] = useState("");
-  const [showLoginForm, setShowLoginForm] = useState(false);
-  const [showRegisterForm, setShowRegisterForm] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [modalMessage, setModalMessage] = useState("");
-
-  const [showAvionImage, setShowAvionImage] = useState(true);
+    const router = useRouter(); // Inicializa useRouter para redireccionar
 
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState(""); // Nuevo estado para el nombre
+    const [location, setLocation] = useState("");
+    const [gender, setGender] = useState("");
+    const [age, setAge] = useState("");
+    const [showLoginForm, setShowLoginForm] = useState(false);
+    const [showRegisterForm, setShowRegisterForm] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [modalMessage, setModalMessage] = useState("");
+    const [showAvionImage, setShowAvionImage] = useState(true);
 
+    // --Login Session--
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
 
-  const router = useRouter(); // Inicializa useRouter para redireccionar
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      console.log('User logged in:', userCredential.user);
-      
-      // Actualizar last_session en Firestore
-      await updateDoc(doc(db, "users", userCredential.user.uid), {
-        last_session: serverTimestamp(), // Guardar la fecha y hora actual
-      });
-
-      // Redirigir a /dashboard después de iniciar sesión exitosamente
-      router.push('/dashboard');
-
-    } catch (error) {
-      console.error('Error logging in:', error);
-      handleAuthError(error);
-    }
-  };
-
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      console.log('User registered:', userCredential.user);
-
-      // Guardar los datos adicionales en Firestore
-      await setDoc(doc(db, "users", userCredential.user.uid), {
-        email: email,
-        display_name: name, // Guardar el nombre bajo el campo display_name
-        ubication: location,
-        gender: gender,
-        age: age,
-        last_session: serverTimestamp(), // Guardar la fecha y hora actual
-      });
-
-      setShowModal(true);
-      setModalMessage("Registro exitoso. ¡Tu cuenta ha sido creada!");
-    } catch (error) {
-      console.error('Error registering:', error);
-      handleAuthError(error);
-    }
-  };
-
-  const handleAuthError = (error) => {
-    let message = "Ocurrió un error. Por favor, intenta de nuevo.";
-    if (error.code === 'auth/wrong-password' || error.code === 'auth/user-not-found') {
-      message = "Contraseña o correo incorrecto. Por favor, verifica tus datos.";
-    } else if (error.code === 'auth/email-already-in-use') {
-      message = "Este correo ya está en uso. Por favor, intenta con otro correo.";
-    }
-    setModalMessage(message);
-    setShowModal(true);
-  };
-
-  const audioRef = useRef(null);
-
-  useEffect(() => {
-    audioRef.current = new Audio('/sound/bottom.m4a');
-  }, []);
-
-  const playSound = () => {
-    if (audioRef.current) {
-      audioRef.current.play();
-    }
-  };
-
-  const handleLoginButtonClick = () => {
-    playSound();
-    setShowLoginForm(true);
-    setShowRegisterForm(false);
-    setShowModal(false);
-  };
-
-  const handleRegisterButtonClick = () => {
-    playSound();
-    setShowRegisterForm(true);
-    setShowLoginForm(false);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-    if (modalMessage === "Registro exitoso. ¡Tu cuenta ha sido creada!") {
-      setShowRegisterForm(false);
-      setShowLoginForm(true);
-    }
-  };
-
-  // -- Animation Anuncio -- 
-
-  useEffect(() => {
-    if (showAvionImage) {
-      gsap.fromTo(
-        "#fotoAvionImg",  
-        { scale: 0, rotation: -90, opacity: 0, x: 100, y: -100 },
-        {
-          scale: 1,
-          rotation: 0,  
-          opacity: 1,
-          x: 0,
-          y: 0,
-          duration: 1.5,
-          ease: "back.out(1.7)",
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+        if (currentUser) {
+          console.log('Usuario autenticado:', currentUser);
+    
+          // Actualizar last_session en Firestore
+          await updateDoc(doc(db, "users", currentUser.uid), {
+            last_session: serverTimestamp(), // Guardar la fecha y hora actual
+          });
+    
+          // Redirigir al dashboard
+          router.push('/dashboard');
+        } else {
+          console.log('No hay usuario autenticado');
+          setLoading(false);
         }
-      );
+      });
+    
+      return () => unsubscribe();
+    }, []);
+
+    if (loading) {
+      return null; // O puedes mostrar un componente de carga si lo prefieres
     }
-  }, [showAvionImage]);
+    
+
+
+    const handleLogin = async (e) => {
+      e.preventDefault();
+      try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        console.log('User logged in:', userCredential.user);
+        
+        // Actualizar last_session en Firestore
+        await updateDoc(doc(db, "users", userCredential.user.uid), {
+          last_session: serverTimestamp(), // Guardar la fecha y hora actual
+        });
+
+        // Redirigir a /dashboard después de iniciar sesión exitosamente
+        router.push('/dashboard');
+
+      } catch (error) {
+        console.error('Error logging in:', error);
+        handleAuthError(error);
+      }
+    };
+
+    const handleRegister = async (e) => {
+      e.preventDefault();
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        console.log('User registered:', userCredential.user);
+
+        // Guardar los datos adicionales en Firestore
+        await setDoc(doc(db, "users", userCredential.user.uid), {
+          email: email,
+          display_name: name, // Guardar el nombre bajo el campo display_name
+          ubication: location,
+          gender: gender,
+          age: age,
+          last_session: serverTimestamp(), // Guardar la fecha y hora actual
+        });
+
+        setShowModal(true);
+        setModalMessage("Registro exitoso. ¡Tu cuenta ha sido creada!");
+      } catch (error) {
+        console.error('Error registering:', error);
+        handleAuthError(error);
+      }
+    };
+
+    const handleAuthError = (error) => {
+      let message = "Ocurrió un error. Por favor, intenta de nuevo.";
+      if (error.code === 'auth/wrong-password' || error.code === 'auth/user-not-found') {
+        message = "Contraseña o correo incorrecto. Por favor, verifica tus datos.";
+      } else if (error.code === 'auth/email-already-in-use') {
+        message = "Este correo ya está en uso. Por favor, intenta con otro correo.";
+      }
+      setModalMessage(message);
+      setShowModal(true);
+    };
+
+    const audioRef = useRef(null);
+
+    
+
+    useEffect(() => {
+      audioRef.current = new Audio('/sound/bottom.m4a');
+    }, []);
+
+    const playSound = () => {
+      if (audioRef.current) {
+        audioRef.current.play();
+      }
+    };
+
+    const handleLoginButtonClick = () => {
+      playSound();
+      setShowLoginForm(true);
+      setShowRegisterForm(false);
+      setShowModal(false);
+    };
+
+    const handleRegisterButtonClick = () => {
+      playSound();
+      setShowRegisterForm(true);
+      setShowLoginForm(false);
+    };
+
+    const handleCloseModal = () => {
+      setShowModal(false);
+      if (modalMessage === "Registro exitoso. ¡Tu cuenta ha sido creada!") {
+        setShowRegisterForm(false);
+        setShowLoginForm(true);
+      }
+    };
+
+    // -- Animation Anuncio -- 
+
+    useEffect(() => {
+      if (showAvionImage) {
+        gsap.fromTo(
+          "#fotoAvionImg",  
+          { scale: 0, rotation: -90, opacity: 0, x: 100, y: -100 },
+          {
+            scale: 1,
+            rotation: 0,  
+            opacity: 1,
+            x: 0,
+            y: 0,
+            duration: 1.5,
+            ease: "back.out(1.7)",
+          }
+        );
+      }
+    }, [showAvionImage]);
   
 
 
@@ -290,16 +320,16 @@ export default function Home() {
             )}
         </div>
 
-        {showAvionImage && (
-            <img
-              id="fotoAvionImg"
-              src="anuncios/anuncio-simifest.svg" // Cambia la ruta a tu imagen real
-              alt="Anuncio de Avión"
-              className="avion-img"
-              onClick={() => setShowAvionImage(false)} // Oculta la imagen al hacer clic
+            {showAvionImage && (
+                <img
+                  id="fotoAvionImg"
+                  src="anuncios/anuncio-simifest.svg" // Cambia la ruta a tu imagen real
+                  alt="Anuncio de Avión"
+                  className="avion-img"
+                  onClick={() => setShowAvionImage(false)} // Oculta la imagen al hacer clic
 
-            />
-          )}
+                />
+              )}
 
     </main>
   );
